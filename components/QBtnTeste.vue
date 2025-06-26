59cfe5424424031276626cc90eaee3c2be18c123
Henrique <henrique.LA@globalweb.com.br>
2025-06-26 09:46:55 -0300

    fix: update repository link in QBtnTeste.vue

--- components/QBtnTeste.vue
+++ components/QBtnTeste.vue
@@ -1,3 +1,3 @@
 <template>
-   <q-btn class="q-mt-md flex" href="https://github.com/Henrique020802/project-nuxt-robson" target="_blank" label="Acesse o repositório no Git" color="primary" />
+   <q-btn class="q-mt-md flex" href="https://github.com/Henrique020802/project-nuxt" target="_blank" label="Acesse o repositório no Git" color="primary" />
 </template>
